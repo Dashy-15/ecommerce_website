0279bf1cabd1321fc8ab85fd723e29a8e7690c92
Subrat <66966741+Evendashiell@users.noreply.github.com>
2025-01-25 22:46:33 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import HomePage from './components/Homepage/HomePage';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
